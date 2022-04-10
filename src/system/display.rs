cb0caaf660d28c23947b86dea48a780ce02f9f84
Mrunmay-arch <mnaphade18@gmail.com>
2022-04-10 17:05:23 +0530

    Make raw terminal Make display mermory write wrap logic wrtie xor logic

--- src/system/display.rs
+++ src/system/display.rs
@@ -4,53 +4,90 @@ mod helpers;
 
 use helpers::display;
 use std::io::{ Write, Stdout, stdout };
-use std::{time, thread };
 use termion::{ color, clear, cursor };
+use termion::raw::{ RawTerminal, IntoRawMode };
 
-#[derive(Debug)]
 pub struct Display {
-    display: Stdout
+    display: RawTerminal<Stdout>,
+    memory: Vec<Vec<bool>>,
 }
 
 impl Display {
     pub fn new() -> Self {
         Display {
-            display: stdout()
+            display: stdout().into_raw_mode().unwrap(),
+            memory: vec! [vec![false; 32]; 64],
         }
     }
     pub fn clear(&mut self) {
         write!(self.display,"{}", clear::All).unwrap();
+        self.memory =  vec! [vec![false; 32]; 64];
     }
 
-    pub fn draw(&mut self, lines: &[u8], x: u16, y: u16) {
+    pub fn print_registers(&mut self, registers: &Vec<u8>) {
+        write!(
+            self.display,
+            "{}Registers: {:?}{}",
+           cursor::Goto(1, 37),
+           registers,
+           cursor::Goto(80, 40),
+        ).unwrap();
+    }
+
+    pub fn draw(&mut self, lines: &[u8], x: u16, y: u16) -> bool {
+        let mut erase = false;
         for (index, line) in lines.iter().enumerate() {
             let index_y = y + index as u16;
-            self.draw_line(*line, x, index_y);
+            erase |= self.draw_line(*line, x, index_y);
         }
-        //thread::sleep(time::Duration::new(1,0));
+
+        erase
     }
     fn set_pixel(&mut self, x: u16, y: u16) {
-        write!(self.display, "{}{} {}", color::Bg(color::Red), cursor::Goto(x, y), color::Bg(color::Reset)).unwrap();
+        write!(
+            self.display,
+           "{}{} {}{}",
+           color::Bg(color::Red),
+           cursor::Goto(x+1, y+1),
+           color::Bg(color::Reset),
+           cursor::Goto(80, 40)
+        ).unwrap();
 
     }
 
     fn unset_pixel(&mut self, x: u16, y: u16) {
-        write!(self.display, "{}{} {}",color::Bg(color::Black), cursor::Goto(x, y), color::Bg(color::Reset)).unwrap();
+        write!(
+            self.display,
+            "{}{} {}{}",
+            color::Bg(color::Black),
+            cursor::Goto(x+1, y+1),
+            color::Bg(color::Reset),
+            cursor::Goto(80, 40),
+        ).unwrap();
     }
 
-    fn draw_line(&mut self, line: u8, x: u16, y: u16) {
+    fn draw_line(&mut self, line: u8, x: u16, unwraped_y: u16) -> bool {
         let binary_string = display::as_binary(line);
+        let mut erase = false;
 
         for (index, bit) in binary_string.chars().enumerate() {
-            let index_x = x + index as u16;
+            let unwraped_x = x + index as u16;
+            let index_x = unwraped_x % 64;
 
-            write!(self.display, "{} Writing for post: {:?} at :{}, {}", cursor::Goto(80, 40), binary_string, index_x, y).unwrap();
+            let y = unwraped_y % 32;
 
-            if bit == '1' {
+            let previous_state = self.memory[index_x as usize][y as usize];
+            self.memory[index_x as usize][y as usize] ^= bit == '1';
+
+            if self.memory[index_x as usize][y as usize] {
                 self.set_pixel(index_x, y);
             } else {
                 self.unset_pixel(index_x, y);
             }
+
+            erase = !self.memory[index_x as usize][y as usize] && !previous_state
         }
+
+        erase
     }
 }
